a3a687e423aa4722fd0c2f517ac85d6fcc63c0e9
Cristinel Panfir <cristinel.panfir@nxp.com>
2022-01-07 12:00:19 +0200

    Add scripts for 5.10.72-2.2.0 release [YOCIMX-5943]
    Update the env link to latest release.
    
    Signed-off-by: Cristinel Panfir <cristinel.panfir@nxp.com>

---
+++ imx-5.10.72-2.2.0/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Here are some default settings.
+# Make sure DOCKER_WORKDIR is created and owned by current user.
+
+# Docker
+
+DOCKER_IMAGE_TAG="imx-yocto"
+DOCKER_WORKDIR="/opt/yocto"
+
+# Yocto
+
+IMX_RELEASE="imx-5.10.72-2.2.0"
+
+YOCTO_DIR="${DOCKER_WORKDIR}/${IMX_RELEASE}-build"
+
+MACHINE="imx8mpevk"
+DISTRO="fsl-imx-xwayland"
+IMAGES="imx-image-core"
+
+REMOTE="https://source.codeaurora.org/external/imx/imx-manifest"
+BRANCH="imx-linux-hardknott"
+MANIFEST=${IMX_RELEASE}".xml"

c169c461f72b30c28e38a65da3a0498642f71e4f
Neena Busireddy <neenareddy.busireddy@nxp.com>
2025-11-10 11:21:20 -0600

    Add scripts for walnascar 6.12.49-2.2.0 release [YOCIMX-9499]
    Update the env link to latest release.
    
    Signed-off-by: Neena Busireddy <neenareddy.busireddy@nxp.com>

---
+++ imx-6.12.49-2.2.0/yocto-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script will run into container
+
+# source the common variables
+
+. imx-6.12.49-2.2.0/env.sh
+
+#
+
+mkdir -p ${YOCTO_DIR}
+cd ${YOCTO_DIR}
+
+# Init
+
+repo init \
+    -u ${REMOTE} \
+    -b ${BRANCH} \
+    -m ${MANIFEST}
+
+repo sync -j`nproc`
+
+# source the yocto env
+
+EULA=1 MACHINE="${MACHINE}" DISTRO="${DISTRO}" source imx-setup-release.sh -b build_${DISTRO}
+
+# Build
+
+bitbake ${IMAGES}
+
